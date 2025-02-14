938e969421f00575796728b35ba7037c7237b3bb
SunnyWen0422 <146903366+SunnyWen0422@users.noreply.github.com>
2025-02-14 23:33:12 +0800

    Add files via upload

---
+++ practice/hello world-app/src/project1/Helloworld.java
@@ -0,0 +1,10 @@
+package project1;
+
+// 这是一个简单的Java程序，打印"Hello World!"
+public class Helloworld {
+    public static void main(String[] args) {
+        // 输出"Hello World!"到控制台
+        System.out.println("Hello World!");
+    }
+}
+
