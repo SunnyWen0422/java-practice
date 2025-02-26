5ae4e9ef23a62af84c98c9e1d5a32872913a64ce
SunnyWen0422 <146903366+SunnyWen0422@users.noreply.github.com>
2025-02-26 23:48:57 +0800

    Add files via upload

---
+++ reflect/src/TestConstructor.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import java.lang.reflect.Constructor;
+public class TestConstructor {
+    @Test
+    public void testGetConstructors(){
+        Class c = Cat.class;
+        //Constructor[] testConstructors = c.getConstructors();
+        Constructor[] testConstructors = c.getDeclaredConstructors();
+        for (Constructor constructor : testConstructors) {
+            System.out.println( constructor.getName()+ "----->"
+            + constructor.getParameterCount());
+
+        }
+    }
+}
